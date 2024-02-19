8281d1a82e868412e327e0294485e748606dce36
Milad Rahimi <info@miladrahimi.com>
2024-02-19 21:52:45 +0100

    update makefile

--- scripts/setup-updater.sh
+++ scripts/setup-updater.sh
@@ -5,7 +5,7 @@ ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/.."
 COMMAND="make -C $ROOT_DIR update"
 
 if ! crontab -l | grep -q "$COMMAND"; then
-    (crontab -l 2>/dev/null; echo "0 4 * * * $COMMAND") | crontab -
+    (crontab -l 2>/dev/null; echo "55 3 * * * $COMMAND") | crontab -
     echo "The updater cron job configured."
 else
     echo "The updater cron job already exists."
