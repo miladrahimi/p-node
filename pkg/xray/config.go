b79ae3f35c6680a91bd7a94c9284e20035a204ee
Milad Rahimi <info@miladrahimi.com>
2025-04-19 19:29:16 +0200

    update

--- pkg/xray/config.go
+++ pkg/xray/config.go
@@ -42,7 +42,7 @@ type OutboundServer struct {
 }
 
 type OutboundSettings struct {
-	Servers []*OutboundServer `json:"servers" validate:"omitempty,dive"`
+	Servers []*OutboundServer `json:"servers,omitempty" validate:"omitempty,dive"`
 }
 
 type StreamSettings struct {
@@ -215,6 +215,7 @@ func NewConfig(logLevel string) *Config {
 		Outbounds: []*Outbound{
 			{
 				Tag:      "out",
+				Settings: &OutboundSettings{},
 				Protocol: "freedom",
 			},
 		},
