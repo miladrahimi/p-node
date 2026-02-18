6017b2a4db56318eb8c16b3f1c985c0614104db2
Milad Rahimi <info@miladrahimi.com>
2026-02-18 06:11:06 +0100

    improve port checking

--- internal/http/handlers/xray/config.go
+++ internal/http/handlers/xray/config.go
@@ -3,13 +3,13 @@ package xray
 import (
 	"fmt"
 	"net/http"
-	"strings"
 
 	"github.com/cockroachdb/errors"
 	"github.com/labstack/echo/v4"
 	"github.com/miladrahimi/p-node/pkg/util"
 	"github.com/miladrahimi/p-node/pkg/xray"
 	"github.com/miladrahimi/p-node/pkg/xray/config"
+	"github.com/miladrahimi/p-node/pkg/xray/config/component"
 )
 
 // ConfigStore stores/updates the Xray configuration.
@@ -27,27 +27,50 @@ func ConfigStore(x *xray.Xray) echo.HandlerFunc {
 			})
 		}
 
+		currentPorts := map[int]int{}
+		for _, inbound := range x.Config().Inbounds {
+			if inbound.Tag == "api" {
+				continue
+			}
+			currentPorts[inbound.Port]++
+		}
+
+		usedPorts := map[int]bool{}
+		var apiInbound *component.Inbound
 		for _, i := range xc.Inbounds {
-			isFree := util.PortFree(i.Port)
-			if i.Tag != "api" && !strings.HasPrefix(i.Tag, "remote-") && !isFree {
+			if i.Tag == "api" {
+				if apiInbound != nil {
+					return ctx.JSON(http.StatusUnprocessableEntity, map[string]string{
+						"message": "Only one API inbound is allowed.",
+					})
+				}
+				apiInbound = i
+				continue
+			}
+
+			if usedPorts[i.Port] {
 				return ctx.JSON(http.StatusUnprocessableEntity, map[string]string{
-					"message": fmt.Sprintf("The port '%d' for '%s' is already in use", i.Port, i.Tag),
+					"message": fmt.Sprintf("The port '%d' is used by multiple inbounds.", i.Port),
 				})
 			}
-			if strings.HasPrefix(i.Tag, "remote-") && !isFree {
-				currentInbound := x.Config().FindInbound(i.Tag)
-				if currentInbound == nil || currentInbound.Port != i.Port {
+			usedPorts[i.Port] = true
+
+			isFree := util.PortFree(i.Port)
+			if !isFree {
+				if currentPorts[i.Port] == 0 {
 					return ctx.JSON(http.StatusUnprocessableEntity, map[string]string{
 						"message": fmt.Sprintf("The port '%d' for '%s' is already in use", i.Port, i.Tag),
 					})
 				}
+				currentPorts[i.Port]--
 			}
-			if i.Tag == "api" {
-				if i.Port, err = util.FreePort(); err != nil {
-					return ctx.JSON(http.StatusUnprocessableEntity, map[string]string{
-						"message": fmt.Sprintf("Cannot generate API inbound port, err: %v", err.Error()),
-					})
-				}
+		}
+
+		if apiInbound != nil {
+			if apiInbound.Port, err = util.FreePort(); err != nil {
+				return ctx.JSON(http.StatusUnprocessableEntity, map[string]string{
+					"message": fmt.Sprintf("Cannot generate API inbound port, err: %v", err.Error()),
+				})
 			}
 		}
 
